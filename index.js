1a386a7d6c6c341b534e106bf59ef7491c8e0e22
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-03-18 21:21:34 -0400

    added in auto slide change

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const slides = [
   {
-    id: 0,
     src: 'images/roquefort-wheels.jpg',
     caption: 'Roquefort is ripened in underground caves for four to nine months'
   },
   {
-    id: 1,
+    src: 'images/roquefort-wheels2.jpg',
+    caption: 'The cheese is kept at a constant 10°C and 95% while ripening'
+  },
+  {
     src: 'images/roquefort-cellar.jpg',
     caption:
       "Roquefort Société's undergound facility at Roquefort-sur-Soulzon, France"
   },
   {
-    id: 2,
     src: 'images/cheesemaker.jpg',
     caption: 'Totally not a real Roquefort production worker!'
+  },
+  {
+    src: 'images/roquefort-wedge.jpg',
+    caption:
+      "A wedge of delicious Roquefort and some random goat's cheese I bought while in France!"
   }
 ]
 
@@ -22,41 +28,63 @@ let slidePosition = 0
 const image = document.getElementById('image')
 const caption = document.getElementById('img-caption')
 const lastSlideIndex = slides.length - 1
+let intervalId
 
-const getPrevSlide = () => {
+document.getElementById('prevSlide').addEventListener('click', getPrevSlide)
+document.getElementById('nextSlide').addEventListener('click', getNextSlide)
+document.addEventListener('animationend', () =>
+  image.classList.remove('animateImg')
+)
+
+startInterval()
+
+function startInterval() {
+  intervalId = setInterval(autoCarousel, 10000)
+}
+
+function resetInterval() {
+  clearInterval(intervalId)
+  startInterval()
+}
+
+function getPrevSlide() {
   if (slidePosition === 0) {
     slidePosition = lastSlideIndex
   } else {
     slidePosition--
   }
 
-  // console.log(slidePosition)
-
-  image.src = slides[slidePosition].src
+  changeSlide()
+  resetInterval()
 }
 
-const getNextSlide = () => {
+function getNextSlide() {
   if (slidePosition === lastSlideIndex) {
     slidePosition = 0
   } else {
     slidePosition++
   }
 
-  // console.log(slidePosition)
-  // image.classList.remove('animateImg')
+  changeSlide()
+  resetInterval()
+}
+
+function autoCarousel() {
+  if (slidePosition === lastSlideIndex) {
+    slidePosition = 0
+  } else {
+    slidePosition++
+  }
+
+  changeSlide()
+}
+
+function changeSlide() {
   image.src = slides[slidePosition].src
   caption.textContent = slides[slidePosition].caption
   image.classList.add('animateImg')
 }
 
-document.getElementById('prevSlide').addEventListener('click', getPrevSlide)
-document.getElementById('nextSlide').addEventListener('click', getNextSlide)
-
 // remove class when CSS animation finishes
-document.addEventListener('animationend', () =>
-  image.classList.remove('animateImg')
-)
-
-setInterval(getNextSlide, 10000)
 
 // image.src = slides[0].src
